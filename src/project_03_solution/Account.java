5a030b2f2a85b6e98dc13fa849e49348134966dc
ChatGTHB <cihat.kose@hotmail.com>
2023-04-28 21:58:49 +0300

    Project 3 solution added.

---
+++ src/project_03_solution/Account.java
@@ -0,0 +1,12 @@
+package project_03_solution;
+
+public class Account {
+
+    public int number;
+    public int funds;
+
+    public Account(int number, int funds) {
+        this.number = number;
+        this.funds = funds;
+    }
+}
