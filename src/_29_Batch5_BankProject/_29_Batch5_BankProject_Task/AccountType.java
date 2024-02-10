e0749ae092ae37323413fb17de5a505ee75430d2
ChatGTHB <cihat.kose@hotmail.com>
2024-02-10 19:09:10 +0100

    Edited

--- src/_29_Batch5_BankProject/_29_Batch5_BankProject_Task/AccountType.java
+++
@@ -1,9 +0,0 @@
-
-
-public enum AccountType {
-
-    // TODO Bu Class'da bir degisiklik yapmaniza gerek yok
-    CHECKING,   // vadesiz hesap
-    SAVINGS,    // tasarruf hesabı
-    CREDIT      // kredi hesabı
-}
