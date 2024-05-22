e9a57077a1fcac8fe07cf02351247eca63fbfd76
ChatGTHB <cihat.kose@hotmail.com>
2024-05-22 20:46:13 +0200

    Edited

--- src/_27_Batch5_LibraryManagementSystem/Kutuphane.java
+++ src/_27_Batch5_LibraryManagementSystem/Kutuphane.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Kutuphane {
 
-
     public static void main(String[] args) {
         Map<String, String> kitaplar = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
